72dded130e24d2a1f06b7779b557318f86a0db62
Amanda Pereira <afp1103@gmail.com>
2017-05-21 17:20:10 -0300

    navigation timing e battery status

--- seminario_doacao/js/index.js
+++ seminario_doacao/js/index.js
@@ -1,3 +1,4 @@
+//GEOLOCATION API
 var localizacao = document.getElementById("localizacao");
 
 if (!navigator.geolocation){
@@ -6,17 +7,59 @@ if (!navigator.geolocation){
 
 function success(position) {
 
-	var latitude  = position.coords.latitude;
-	var longitude = position.coords.longitude;
+	var latitude  = position.coords.latitude; //obtem a latitude atual
+	var longitude = position.coords.longitude; //obtem a longitude atual
 
+	//exibe a posição atual em termos de latitude e longitude
 	localizacao.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
 }
 
-function error() {
+function error() { //usada quando o usuário bloqueia o uso da localização
 	localizacao.innerHTML = "Não foi possível obter sua localização";
 }
 
 localizacao.innerHTML = "<p>Buscando localização...</p>";
 
 navigator.geolocation.getCurrentPosition(success, error);
+
+
+//NAVIGATION TIMING API
+//tempo de carregamento da página
+var horaAtual = new Date().getTime();
+var tempoDeCarregamento = horaAtual - performance.timing.navigationStart;
+
+//tempo de requisicao-resposta
+var tempoDeConexao = performance.timing.responseEnd - performance.timing.requestStart;
+
+console.log("Tempo de carregamento da página: " + tempoDeCarregamento + "ms");
+console.log("Tempo de conexão: " + tempoDeConexao + "ms");
+
+//BATTERY STATUS
+function updateBatteryStatus(battery) {
+
+	var nivelBateria = battery.level * 100;
+
+	var textoBateria = "Nível de bateria: " + nivelBateria + "% ";
+
+	document.querySelector('#bateria').textContent = textoBateria;
+	document.querySelector('#bateria').append(battery.charging ? 'Carregando...' : ' ');
+}
+
+navigator.getBattery().then(function(battery) {
+    // Update the battery status initially when the promise resolves ...
+    updateBatteryStatus(battery);
+
+    // .. and for any subsequent updates.
+    battery.onchargingchange = function () {
+    	updateBatteryStatus(battery);
+    };
+
+    battery.onlevelchange = function () {
+    	updateBatteryStatus(battery);
+    };
+
+    battery.ondischargingtimechange = function () {
+    	updateBatteryStatus(battery);
+    };
+});
